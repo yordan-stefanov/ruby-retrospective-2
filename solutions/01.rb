14a48eaf2bc5bd05d3910d9d5eb045bfabb302d3
Yordan Stefanov <jovenstef@gmail.com>
2012-12-30 17:55:23 +0200

    hw 01: added Range#fizzbuzz

--- solutions/01.rb
+++ solutions/01.rb
@@ -12,3 +12,14 @@ class Integer
     # but it requires "Prime"
   end
 end
+
+class Range
+  def fizzbuzz
+    self.map &->( element ) do
+      return :fizzbuzz if element % 15 == 0
+      return :fizz if element % 3 == 0
+      return :buzz if element % 5 == 0
+      element
+    end
+  end
+end
